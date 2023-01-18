fd851857c1a39e01a4a926abb3dbc80fc87750d3
jakejurado <97064241+jakejurado@users.noreply.github.com>
2023-01-17 20:45:44 -0800

    GildedRose-completed

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,5 +1,16 @@
+// BEST PRACTICES
+// used a linter according to the AirBnB linting standard
+// improved readability and logic by removing nested if statements from the updateQuality method and implemented a switch.
+// modularized the code for scalability by creating new methods for each item type in the store.
+// Implemented variables (such as maxQuality, minQuality, and rate) in the Shop class to make future refactoring easier  (side note, I was not able to do this for the backstage tickets rate increases without making some assumptions, so I left as is)
+// separated out the sellIn updater into its own method for the same reasons stated above.
+// I added comments to help future developers know what's going on.
+// I wrote descriptive code to make everything easier to read.
+// I wrote tests to make sure the refactored code works as expected.
+// I wrote tests to make sure the new update works as expected.
+
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,61 +18,94 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
+    this.maxQuality = 50;
+    this.minQuality = 0;
+    this.rate = 1;
   }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
 
-    return this.items;
+  updateBrie(item) {
+    //increment quality unless maxQuality has been reached
+    item.quality =
+      item.quality >= this.maxQuality
+        ? this.maxQuality
+        : item.quality + this.rate;
+  }
+
+  updatePasses(item) {
+    //if concert has passed then item quality is 0
+    if (item.sellIn < this.minQuality) item.quality = this.minQuality;
+    else {
+      //update quality depending on how close we are to concert (never past maxQuality)
+      item.quality++;
+      if (item.sellIn < 10) item.quality++;
+      if (item.sellIn < 5) item.quality++;
+      item.quality =
+        item.quality > this.maxQuality ? this.maxQuality : item.quality;
+    }
+  }
+
+  updateSulfuras(item) {
+    //no logic needed
+  }
+
+  updateConjured(item) {
+    //quality declines double the normal rate and double again if sellIn is negative (quality is never negative)
+    const doubleRate = this.rate * 2;
+    let newRate = item.sellIn < 0 ? doubleRate * 2 : doubleRate;
+    item.quality =
+      item.quality - newRate < this.minQuality
+        ? this.minQuality
+        : item.quality - newRate;
+  }
+
+  updateItem(item) {
+    //quality declines and then doubles the rate if sellIn is negative (quality is never negative)
+    const newRate = item.sellIn < 0 ? this.rate * 2 : this.rate;
+    item.quality =
+      item.quality - newRate < this.minQuality
+        ? this.minQuality
+        : item.quality - newRate;
+  }
+
+  updateSellIn(item) {
+    //reduce sellIn by one for every item except Sulfuras
+    item.sellIn =
+      item.name !== "Sulfuras, Hand of Ragnaros"
+        ? item.sellIn - this.rate
+        : item.sellIn;
+  }
+
+  //iterate over all items to update quality and sellIn values
+  updateQuality() {
+    for (let item of this.items) {
+      //update sellIn value
+      this.updateSellIn(item);
+
+      //update quality value
+      switch (item.name) {
+        case "Aged Brie":
+          this.updateBrie(item);
+          break;
+        case "Backstage passes to a TAFKAL80ETC concert":
+          this.updatePasses(item);
+          break;
+        case "Sulfuras, Hand of Ragnaros":
+          break;
+        case "Conjured":
+          this.updateConjured(item);
+          break;
+        default:
+          this.updateItem(item);
+          break;
+      }
+      return this.items;
+    }
   }
 }
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,105 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+describe("Gilded Rose", function () {
+  it("quality of Brie cheese grows by 1 every day", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(11);
+  });
+
+  it("quality of Brie cheese and Tickets never grow past 50", () => {
+    const gildedRose = new Shop([
+      new Item("Aged Brie", 4, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 50),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBeLessThan(51);
+    expect(gildedRose.items[1].quality).toBeLessThan(51);
+  });
+
+  it("sellIn of Brie cheese shrinks by 1 every day (even though it doesn't have a purpose)", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(0);
+  });
+
+  it("quality of backstage drops each day by 1 when sellIn is above 10", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 11, 0),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(1);
+  });
+
+  it("quality of backstage drops each day by 2 when sellIn is above 5 and below 11", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(2);
+  });
+
+  it("quality of backstage drops each day by 1 when sellIn is above 0 and below 6", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(3);
+  });
+
+  it("quality of backstage passes are 0 after sellIn is negative", () => {
+    const gildedRose = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
+
+  it("sulfuras never decresses in quality", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 1, 80),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(80);
+  });
+
+  it("sulfuras' sellIn value never decreases (although it has no purpose)", () => {
+    const gildedRose = new Shop([
+      new Item("Sulfuras, Hand of Ragnaros", 1, 80),
+    ]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].sellIn).toBe(1);
+  });
+
+  it("each day lowers both quality and sellin", () => {
+    const gildedRose = new Shop([new Item("toothpaste", 5, 5)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(4);
+    expect(gildedRose.items[0].sellIn).toBe(4);
+  });
+
+  it("quality of an item should never be negative", () => {
+    const gildedRose = new Shop([new Item("toothpaste", 5, 1)]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
+
+  it("quality degrades twice as fast once sell date has passed", () => {
+    const gildedRose = new Shop([new Item("toothpaste", 0, 20)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(18);
+  });
+
+  it("Conjured items degrade in Quality twice as fast as normal items", () => {
+    const gildedRose = new Shop([new Item("Conjured", 10, 20)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(18);
+  });
+
+  it("Conjured items degrade in Quality twice as fast as normal items when sellIn is negative", () => {
+    const gildedRose = new Shop([new Item("Conjured", 0, 20)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(16);
   });
 });
